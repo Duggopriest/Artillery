fea543789adc8dd639c11140ac3cbb6b50ef4aa9
Jaymie Gobbett <jjjj7350@gmail.com>
2023-06-12 15:25:28 +0930

    added shell physics so it down moves in a relistic arc

--- artillery.js
+++ artillery.js
@@ -4,17 +4,20 @@ var context = canvas.getContext("2d");
 var SCREEN_WIDTH = canvas.width;
 var SCREEN_HEIGHT = canvas.height;
 
+var	time = 0;
 var deltaTime = 0;
+var gravity = -9.80665;
 var STATE_GAME = 0;
 
 var splashTimer = .5;
 
+
 var SHELL = {
 	image: document.createElement("img"),
-	x: 0.0,
-	y: 0.0,
-	Accx: 0.0,
-	Accy: 0.0,
+	pos: new Vec(0.0,0.0,0.0),
+	acc: new Vec(),
+	angle: 45,
+	speed: 100.0,
 	width: 50,
 	height: 50,
 	fired: false
@@ -51,18 +54,20 @@ function getDeltaTime()
     return deltaTime;
 }  
 
-function drawGround()
+function calBullet(deltaTime, time)
 {
-	if (SHELL.y < 500)
-		return false;
-	var x = SHELL.x;
-	var y = SHELL.y;
-	for (var i = -50; i < canvas.width + 50; i += 50)
-	{
-		context.drawImage(SHELL.image, 500, (i + x) % canvas.width, 50, 50);
-	}
-	console.log("working");
-	return true;
+	// var point = SHELL.pos.add( SHELL.acc.multiply(SHELL.speed * time * 0.03 ) );
+	
+	// var VecDown = new Vec(0.0, -1.0);
+	// var gravityVec = VecDown.multiply(gravity * time * time);
+
+	// SHELL.pos = point.add(gravityVec);
+
+	// SHELL.pos.x = SHELL.acc.x * time;
+	// SHELL.pos.y = SHELL.acc.y + (gravity * time);
+
+	SHELL.pos.x = SHELL.acc.x * time;
+	SHELL.pos.y = SHELL.acc.y * time + gravity / 2 * time * time;
 }
 
 function drawScene()
@@ -71,29 +76,31 @@ function drawScene()
 	{
 		for (var j = -600; j < 1000; j += 600)
 		{
-			context.drawImage(Stars, -(SHELL.x % 600) + i, (SHELL.y % 600) + j, 600, 600);
+			context.drawImage(Stars, -(SHELL.pos.x % 600) + i, (SHELL.pos.y % 600) + j, 600, 600);
 		}
 	}
 	for (var i = -1921; i < canvas.width + 1921; i += 1921)
 	{
-		context.drawImage(Mountans, -(SHELL.x * 3 % 1921) + i, SHELL.y + 500, 1921, 500);
+		context.drawImage(Mountans, -(SHELL.pos.x * 3 % 1921) + i, SHELL.pos.y + 500, 1921, 500);
 	}
 	for (var i = -1500; i < canvas.width + 1500; i += 1500)
 	{
-		context.drawImage(Tree, -(SHELL.x * 6 % 1500) + i, SHELL.y + 710, 1500, 300);
+		context.drawImage(Tree, -(SHELL.pos.x * 6 % 1500) + i, SHELL.pos.y + 710, 1500, 300);
 	}
 	for (var i = -150; i < canvas.width + 150; i += 150)
 	{
-		context.drawImage(Grass, -(SHELL.x * 10 % 150) + i, SHELL.y + 990, 150, 150);
+		context.drawImage(Grass, -(SHELL.pos.x * 10 % 150) + i, SHELL.pos.y + 990, 150, 150);
 	}
 	
 	// draw bullet
 	if (SHELL.fired)
-		context.drawImage(SHELL.image, 500, 500, 50, 50);
+	{
+		context.drawImage(SHELL.image, 500, (SHELL.pos.y > 500 ? 500 : (-SHELL.pos.y + 1000)), 50, 50);
+	}
 	else
 	{
 		//context.scale(-1,1);
-		context.drawImage(Cannon, 500, SHELL.y + 950, -271, 81);
+		context.drawImage(Cannon, 500, SHELL.pos.y + 950, -271, 81);
     
     	// always clean up -- reset transformations to default
     	context.setTransform(1,0,0,1,0,0);
@@ -119,6 +126,13 @@ function runSplash(deltaTime)
         context.fillText("By Jaymie Gobbett", 10, 60);
 }
 
+var startFire = 0;
+function getTime()
+{
+    return (Date.now()  * 0.001) - startFire;
+}
+
+var s = true;
 function runGame(deltaTime)
 {
     context.fillStyle = "#000";
@@ -126,10 +140,34 @@ function runGame(deltaTime)
 
 	drawScene();
 
-	if (SHELL.fired)
+	
+	if (SHELL.fired && SHELL.pos.y > -5)
 	{
-		SHELL.x += deltaTime * 100;
-		SHELL.y += deltaTime * 50;
+		if (startFire == 0)
+		{
+			SHELL.acc.x = Math.cos(SHELL.angle * Math.PI / 180) * SHELL.speed;
+			SHELL.acc.y = Math.sin(SHELL.angle * Math.PI / 180) * SHELL.speed;
+
+
+			// var angle = Math.atan(SHELL.acc.y / SHELL.acc.x) * 180.0 / Math.PI;
+			// console.log("current angle = " + angle);
+
+			// var range = SHELL.speed * SHELL.speed * Math.sin(2.0 * angle) / gravity; // https://www.omnicalculator.com/physics/range-projectile-motion
+			// console.log("predicted distance = " + range);
+
+			console.log(SHELL.acc);
+
+			console.log(SHELL.pos);
+			startFire = Date.now()  * 0.001;
+		}
+		calBullet(deltaTime, getTime());
+		
+	}
+	else if (SHELL.fired && SHELL.pos.y <= -5 && s)
+	{
+		console.log(SHELL.pos);
+		console.log("time = " + getTime());
+		s = false;
 	}
 	
 }
